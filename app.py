d654b66a55ed4faad68811209c43b16f4328b595
Max Base <MaxBaseCode@gmail.com>
2022-03-26 03:20:58 +0430

    complete bot, looking for python tag

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 USERNAME = "max.base"
 PASSWORD = "xxxxxxxxxxxxxxxxxx"
-KEYWORD = "#cat"
+KEYWORD = "#python"
 
 driver = webdriver.Firefox()
 
@@ -23,3 +23,46 @@ username.send_keys(USERNAME)
 password.clear()
 password.send_keys(PASSWORD)
 
+button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
+
+time.sleep(5)
+alert = WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
+
+# Searching for a hashtag
+searchbox = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@placeholder='Search']")))
+searchbox.clear()
+searchbox.send_keys(KEYWORD)
+ 
+time.sleep(5) # Wait for 5 seconds
+my_link = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//a[contains(@href, '/" + keyword[1:] + "/')]")))
+my_link.click()
+
+# Scroll down 2 times
+n_scrolls = 2
+for j in range(0, n_scrolls):
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(5)
+
+anchors = driver.find_elements_by_tag_name('a')
+anchors = [a.get_attribute('href') for a in anchors]
+anchors = [a for a in anchors if str(a).startswith("https://www.instagram.com/p/")]
+print('Found ' + str(len(anchors)) + ' links to images')
+
+images = []
+for a in anchors:
+    driver.get(a)
+    time.sleep(5)
+    img = driver.find_elements_by_tag_name('img')
+    img = [i.get_attribute('src') for i in img]
+    images.append(img[1])
+    
+path = os.getcwd()
+path = os.path.join(path, keyword[1:] + "s")
+os.mkdir(path)
+
+# Download images
+counter = 0
+for image in images:
+    save_as = os.path.join(path, keyword[1:] + str(counter) + '.jpg')
+    wget.download(image, save_as)
+    counter += 1
