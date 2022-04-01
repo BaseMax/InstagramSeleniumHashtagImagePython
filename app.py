b23254557340163de01721138959496468977d16
sbhatm1213 <sbhatm1213@gmail.com>
2022-04-01 23:30:18 +0530

    Updated app.py
    (1) Added implicitly-wait 15, this will be taken as default for every find element call.
    (2) Added click "Not now" for the Turn-on-Notification popup

--- app.py
+++ app.py
@@ -15,8 +15,11 @@ driver = webdriver.Firefox()
 
 driver.get("http://www.instagram.com")
 
-username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
-password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
+# Default wait of 15 for every find element
+driver.implicitly_wait(15)
+
+username = driver.find_element(By.CSS_SELECTOR, "input[name='username']")
+password = driver.find_element(By.CSS_SELECTOR, "input[name='password']")
 
 username.clear()
 username.send_keys(USERNAME)
@@ -25,17 +28,19 @@ password.send_keys(PASSWORD)
 
 button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
 
-time.sleep(5)
-alert = WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
+# Click "NOT NOW" when asked for "Save login"
+save_login_or_not_popup = driver.find_element(By.XPATH, '//button[contains(text(), "Not Now")]').click()
+
+# Click "NOT NOW" when asked for "Desktop notification"
+get_notification_or_not_popup = driver.find_element(By.XPATH, '//button[contains(text(), "Not Now")]').click()
 
 # Searching for a hashtag
-searchbox = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@placeholder='Search']")))
+searchbox = driver.find_element(By.XPATH, "//input[@placeholder='Search']")
 searchbox.clear()
 searchbox.send_keys(KEYWORD)
- 
-time.sleep(5) # Wait for 5 seconds
-my_link = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//a[contains(@href, '/" + keyword[1:] + "/')]")))
-my_link.click()
+
+my_link = driver.find_element(By.XPATH, "//a[contains(@href, '/" + KEYWORD[1:] + "/')]").click()
+
 
 # Scroll down 2 times
 n_scrolls = 2
@@ -43,7 +48,7 @@ for j in range(0, n_scrolls):
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     time.sleep(5)
 
-anchors = driver.find_elements_by_tag_name('a')
+anchors = driver.find_elements(By.TAG_NAME, 'a')
 anchors = [a.get_attribute('href') for a in anchors]
 anchors = [a for a in anchors if str(a).startswith("https://www.instagram.com/p/")]
 print('Found ' + str(len(anchors)) + ' links to images')
@@ -52,7 +57,7 @@ images = []
 for a in anchors:
     driver.get(a)
     time.sleep(5)
-    img = driver.find_elements_by_tag_name('img')
+    img = driver.find_elements(By.TAG_NAME, 'img')
     img = [i.get_attribute('src') for i in img]
     images.append(img[1])
     
